b5061b924342aa6c0febf9b1e36cec6b850aadfe
Dayan Bravo <dayan3847@gmail.com>
2022-11-02 14:37:26 -0600

    feat: use input for parameters

--- primera_tanda_de_ejercicios/emd/ej9/src/run_tin_marin.py
+++ primera_tanda_de_ejercicios/emd/ej9/src/run_tin_marin.py
@@ -2,14 +2,24 @@ from tinmarin import TinMarin
 
 
 def run_tin_marin():
-    phrases_count: int = 3
-    people_count: int = 10
+    phrases_count: int = int(input('Insert the number of phrases in the couplet: '))
+    people_count: int = int(input('Insert the number of people: '))
+
     tin_marin = TinMarin(phrases_count, people_count)
+
+    print('People in the circle:')
+    print(tin_marin.people_circle)
+
+    team1 = []
 
     fist_team_size: int = int(people_count / 2)
     for i in range(0, fist_team_size):
         i_person = tin_marin.select_next_person()
-        print(i_person)
+        team1.append(i_person)
+    print('Team1:')
+    print(team1)
+    print('Team2:')
+    print(tin_marin.people_circle)
 
 
 if __name__ == '__main__':
