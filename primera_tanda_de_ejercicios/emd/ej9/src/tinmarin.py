1c1c0c738351475f321e50b333c96c43c1c41fea
Dayan Bravo <dayan3847@gmail.com>
2022-11-03 15:12:19 -0600

    refactor: merge tin marin

--- primera_tanda_de_ejercicios/emd/ej9/src/tinmarin.py
+++ primera_tanda_de_ejercicios/emd/ej9/src/tinmarin.py
@@ -2,7 +2,6 @@ from typing import List
 
 
 class TinMarin:
-
     __last_select_person_index: int
 
     __phrases_count: int
@@ -22,3 +21,28 @@ class TinMarin:
         next_person_index_mod = next_person_index % people_count
         self.__last_select_person_index = next_person_index_mod
         return self.people_circle.pop(next_person_index_mod)
+
+
+def run_tin_marin():
+    phrases_count: int = int(input('Insert the number of phrases in the couplet: '))
+    people_count: int = int(input('Insert the number of people: '))
+
+    tin_marin = TinMarin(phrases_count, people_count)
+
+    print('People in the circle:')
+    print(tin_marin.people_circle)
+
+    team1 = []
+
+    fist_team_size: int = int(people_count / 2)
+    for i in range(0, fist_team_size):
+        i_person = tin_marin.select_next_person()
+        team1.append(i_person)
+    print('Team1:')
+    print(team1)
+    print('Team2:')
+    print(tin_marin.people_circle)
+
+
+if __name__ == '__main__':
+    run_tin_marin()
